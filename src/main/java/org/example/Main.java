92ed7a7028209aa5ad9609481dd0506e917902fd
krugliashok08 <annakm803@gmail.com>
2024-03-13 17:03:41 +0300

    Lesson_3.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -112,14 +112,12 @@ public class Main {
 
 
     public static void Task9 () {
-        int[][] arr = new int[5][5];
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0, x = arr[i].length - 1; j < arr[i].length; j++, x--) {
-                if (i == j || i == x) arr[i][j] = 1;
-                else arr[i][j] = 0;
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.print("\n");
+        int n = 4;
+        int[][] array = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            array[i][i] = 1;
+            array[i][n - i - 1] = 1;
         }
 
     }
